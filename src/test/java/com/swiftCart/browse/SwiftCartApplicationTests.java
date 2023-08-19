dcaddd7a5c65ff0e6714eea2c15a164ab886bee4
Malay Biswal <malbiswa@publicisgroupe.net>
2023-08-20 00:09:09 +0530

    Initial commit

---
+++ src/test/java/com/swiftCart/browse/SwiftCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftCart.browse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
